0c91a93333cabd5ee56e1ef0d4325740f8f7a268
GG <gaelgalletas@gmail.com>
2025-04-01 18:35:32 -0700

    Agregue un codigo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import{ useState} from 'react'
+
+import'./App.css'
+
+import AnimalsShow from './AnimalsShow'
+
+function getRamdonAnimal() {
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.ramdon() * animals.length)]
+
+}
+
+console.log(getRamdonAnimal())
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () => {
+    setAnimals ([...animals, getRamdonAnimal()])
+  }
+  const renderAnimals = animals.map((animal, index)=>{
+    return <AnimalsShow type={animal} key = {index}/>
+  })
 
   return (
-    <>
-      <h1> animals </h1>
-    </>
+
+    <div className='app'>
+      <button onClick={handleClick}>Add Animmal</button>
+    <div className='animal-list'>
+
+    </div>
+    </div>
   )
 }
 
